ecec7b4c14a5475fb45ca41949c0f30740e7ad4c
AniMeloyan <135531912+AniMeloyan@users.noreply.github.com>
2024-11-25 23:40:47 +0400

    Add files via upload

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using LoginForm.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginForm.Controllers
+{
+    public class AuthController : Controller
+    {
+        public AuthController() {
+            ViewData["Valod"] = "Hajox";
+        }
+        public IActionResult Index()
+        {
+           return RedirectToAction(nameof(Login1));
+        }
+
+        [HttpGet]
+        public IActionResult Login1()
+        {
+            return View("Login");
+        }
+        [HttpPost]
+        public IActionResult Login (Login field)
+        {
+            var users = new[]
+            {
+                new { Email = "ani.meloyan@mail.ru", Password = "pass123" },
+                new { Email = "user@example.com", Password = "password123" }
+            };
+
+            var user = users.FirstOrDefault(u => u.Email == field.Email && u.Password == field.Password);
+            if (user != null)
+       
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid email or password.";
+
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginForm.Models
+{
+    public class Login
+    {
+              
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
